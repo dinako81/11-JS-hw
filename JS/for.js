02705d4ec966cff47567b5727581dfc6ae823973
dinako81 <dina.koliata@gmail.com>
2023-01-18 20:01:30 +0200

    updates

--- JS/for.js
+++ JS/for.js
@@ -6,28 +6,26 @@
 // e.	-50 … 50
 // f.	-70 … 30
 
-
+console.log('-------0...0-------')
 // a.	0 … 0
+let sumA = 0;
 
-const skaiciaiA = [0]
-let sum = 0;
-
-for (let i = 0; i < skaiciaiA.length; i++) {
-    console.log(skaiciaiA[i])
+for (let i = 0; i <= 0; i++) {
+    sumA += i; 
 }
+console.log('Suma: ', sumA);
 
+console.log('-------0...4-------');
 
 // b.	0 … 4
-
-const skaiciaiB = [0, 1, 2, 3, 4]
-
 let sumB = 0;
 
 for (let i = 0; i <= 4; i++) {
     sumB += i;
 }
-console.log('suma:', sumB)
+console.log('suma:', sumB);
 
+console.log('-------0...100-------');
 // c.	0 … 100
 
 let sumC = 0;
@@ -35,4 +33,31 @@ let sumC = 0;
 for (let i = 0; i <= 100; i++) {
     sumC += i;
 }
-console.log('suma:', sumC)
+console.log('suma:', sumC);
+
+console.log('-------574...815-------');
+// d.	574 … 815
+let sumD = 0;
+
+for (let i = 574; i <= 815; i++) {
+    sumD += i;
+}
+console.log('suma:', sumD)
+
+console.log('--------(-50)...50-------');
+// e.	-50 … 50
+let sumE = 0;
+
+for (let i = -50; i <= 50; i++) {
+    sumE += i;
+}
+console.log('suma:', sumE)
+
+console.log('--------(-70)...30-------');
+// f.	-50 … 50
+let sumF = 0;
+
+for (let i = -70; i <= 30; i++) {
+    sumF += i;
+}
+console.log('suma:', sumF)

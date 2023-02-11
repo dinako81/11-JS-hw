1f4436a67a9eaaf5f71bc68a14478d8a6ede23d1
dinako81 <dina.koliata@gmail.com>
2023-02-12 00:18:10 +0200

    updates

---
+++ JS/objects.js
@@ -0,0 +1,26 @@
+//sudaryti funkcija
+//Pasiimkite masyva students ir ispausdinkite console: Vardenis: 8. – 
+// irasysite konkretu varda Petras ir suskaiciuosite pazymiu vidurki. 
+// Tada Maryte, Jona. Parasykite funkcija, kuri, kai gauna toki masyva 
+// (students) su viduje kompleksine informacija, 
+// dirbdama turi eiti cikliskai per kiekviena masyvo nari, 
+// is jo issitraukia objekta, is jo issitraukia varda, tada 
+// pazymius – kiekviena sarasa, is jo vdurki isveda, ir 
+// galutini atsakyma isspausdina – toks vidurkis, kitas 
+// vardenis – toks vidurkis, kaip atskiras eilutes kiekvieno. 
+
+
+const students = [
+    {
+        name: 'Petras',
+        marks: [10, 2, 8, 4, 6],
+    },
+    {
+        name: 'Maryte',
+        marks: [2, 4, 6, 8, 10, 2, 4, 8],
+    },
+    {
+        name: 'Jonas',
+        marks: [9, 8, 7],
+    },
+];
